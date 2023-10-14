e0eebd8c2ca24c0dec43f5305e71a7dba5321867
Joses Syanwa <josesyanwa@gmail.com>
2023-10-14 20:00:00 +0300

    completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then((response) => {
   
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+    
+      return response.json();
+    })
+    .then((data) => {
+    
+      renderBooks(data);
+    })
+    .catch((error) => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+  const ul = document.createElement('ul');
+
+  books.forEach((book) => {
+    const li = document.createElement('li');
+    li.textContent = book.name;
+    ul.appendChild(li);
   });
+
+  main.appendChild(ul);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+fetchBooks();
